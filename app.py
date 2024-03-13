953413a1a7c5ad9eb7edb6582c9cecc608d055a3
Agampreet Kaur <122470353+kaurAgam@users.noreply.github.com>
2024-03-13 05:00:16 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask  # creating a class
+from flask import Flask, render_template  # creating a class
 
 app = Flask(__name__)  # creating object of class
+
+poems = [
+    {
+        'id': 1,
+        'title': 'Are we Home yet?',
+        'description': 'abc',
+        'link': 'https://www.poetryfoundation.org/poems/46464/the-raven-5874d'
+    },
+    {
+        'id': 2,
+        'title': 'The Raven',
+        'description': 'abc',
+        'link': 'https://www.poetryfoundation.org/poems/46464/the-raven-5874d'
+    },
+    {
+        'id': 3,
+        'title': 'The Raven',
+        'description': 'abc',
+        'link': 'https://www.poetryfoundation.org/poems/46464/the-raven-5874d'
+    },
+    {
+        'id': 4,
+        'title': 'The Raven',
+        'description': 'abc',
+        'link': 'https://www.poetryfoundation.org/poems/46464/the-raven-5874d'
+    },
+]
 
 
 @app.route("/")  # url route
 def hello_world():  # function name
-  return "<p>Hello, World!</p>"
+  return render_template('home.html', poems=poems)
 
 
 if __name__ == "__main__":
